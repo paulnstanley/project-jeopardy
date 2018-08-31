f9a7bf842c35f7a9e08427831852925f89b7d3af
Charles Ferringer <charlesferringer@yahoo.com>
2018-08-31 14:59:47 -0400

    Works. Ish.

--- src/App.js
+++ src/App.js
@@ -9,13 +9,26 @@ import "./index.css";
 class App extends Component {
   constructor() {
     super()
-
+    this.tableValues = [
+        {
+          value: '$200'
+        }, {
+          value: '$400'
+        }, {
+          value: '$600'
+        }, {
+          value: '$800'
+        }, {
+          value: '$1000'
+        }
+      ]
     this.state = {
       hasData:false,
       columns: [
         {
           Header: 'Category 1',
           accessor: 'value',
+          id: '1',
           headerStyle: {
             marginTop: '0px',
             height: '75px'
@@ -83,42 +96,73 @@ class App extends Component {
   }
 
   updateColumnTitles = function (newState) {
-    // console.log(newState)
-    // console.log(this.state.columns)
+    console.log(newState[0].clues[0].value)
+    console.log(this.state.columnData[0].clues.value)
 
-    const columnHeaders = [{
+    const columnHeaders = [
+      {
+
       Header: newState[0].category,
+      // Cell: "400",
       accessor: 'value',
+      Cell: '500',
+      id: '1',
       headerStyle: {
         marginTop: '0px',
-        height: '100px'
+        height: '80px'
       },
       style: {
-        borderRadius: "5px",
-        borderColor: "black"
+        marginTop: '10px',
+        fontSize: '20px'
       }
     },
     {
       Header: newState[1].category,
-      accessor: 'value'
+      accessor: 'value',
+      Cell: '500',
+      style: {
+        marginTop: '10px',
+        fontSize: '20px'
+      }
     },
     {
       Header: newState[2].category,
-      accessor: 'value'
+      accessor: 'value',
+      Cell: '500',
+      style: {
+        marginTop: '10px',
+        fontSize: '20px'
+      }
     },
     {
       Header: newState[3].category,
-      accessor: 'value'
+      accessor: 'value',
+      Cell: '500',
+      style: {
+        marginTop: '10px',
+        fontSize: '20px'
+      }
     },
     {
       Header: newState[4].category,
-      accessor: 'value'
+      accessor: 'value',
+      Cell: '500',
+      style: {
+        marginTop: '10px',
+        fontSize: '20px'
+      }
     },
     {
       Header: newState[5].category,
-      accessor: 'value'
+      accessor: 'value',
+      Cell: '500',
+      style: {
+        marginTop: '10px',
+        fontSize: '20px'
+      }
     }
   ]
+
   this.setState({columns: columnHeaders})
   }
 
@@ -175,12 +219,12 @@ class App extends Component {
         <Column />
         <div className="container">
           <div className="row">
-            <div className="col-md-2">
+            <div className="col-md-1">
               <div className="score">
                 Player 1 Score: threeve
               </div>
             </div>
-            <div className="col-md-8" id="table">
+            <div className="col-md-10" id="table">
               <ReactTable
                 style={{
                   height:"500px",
@@ -202,7 +246,7 @@ class App extends Component {
                 getTdProps={getTdProps}
               />
             </div>
-            <div className="col-md-2">
+            <div className="col-md-1">
             <div className="score">
               Player 2 Score: v
             </div>
